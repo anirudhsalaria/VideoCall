03ceacba79e736b48c4fccc9ca775fe499752f08
Anirudh Salaria <salariaanirudh@gmail.com>
2023-05-31 21:46:51 +0530

    update

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,9 +3,26 @@ import React from 'react'
 export default function HomePage() {
     return (
         <>
+            {/* ----------------------------------------------------------------------------------------------------------------- */}
             <div>
-                <h1>Home page</h1>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <a className="navbar-brand" href="/">VideoCall</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="/">Home</a>
+                            </li>
+                        </ul>
+                        
+                    </div>
+                </nav>
             </div>
+            {/* ----------------------------------------------------------------------------------------------------------------- */}
+
+
         </>
     )
 }
