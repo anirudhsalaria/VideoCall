32a72b5598d19f3f052976f5846a4a571db859af
Anirudh Salaria <salariaanirudh@gmail.com>
2023-06-02 13:55:37 +0530

    RoomPage added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom';
 import HomePage from './pages/Home';
+import RoomPage from './pages/Room';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<HomePage/>}/>
+      <Route path='/room/:roomId' element={<RoomPage/>}/>
     </Routes>
   );
 }
---
+++ src/pages/Room/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function RoomPage() {
+    return (
+        <div>
+            <>
+                <h1>Room</h1>
+            </>
+        </div>
+    )
+}
