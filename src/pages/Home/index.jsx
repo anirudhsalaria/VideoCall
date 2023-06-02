a9ca7199313fe00fc1e98f142261e30d0790a982
Anirudh Salaria <salariaanirudh@gmail.com>
2023-06-02 13:28:46 +0530

    Update index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,17 +6,18 @@ export default function HomePage() {
             {/* ----------------------------------------------------------------------------------------------------------------- */}
             <div>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <a className="navbar-brand" href="/">VideoCall</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item active">
-                                <a className="nav-link" href="/">Home</a>
-                            </li>
-                        </ul>
-
+                    <div className="container-fluid">
+                        <a className="navbar-brand" href="/">VideoCall</a>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav mr-auto">
+                                <li className="nav-item active">
+                                    <a className="nav-link" href="/">Home</a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </nav>
             </div>
