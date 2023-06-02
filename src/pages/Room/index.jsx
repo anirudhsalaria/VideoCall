ad5389bedb38343b852dde51355dbb838ff7c081
Anirudh Salaria <salariaanirudh@gmail.com>
2023-06-02 14:44:19 +0530

    ZegoCloud integrated

--- src/pages/Room/index.jsx
+++ src/pages/Room/index.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
 
 export default function RoomPage() {
+    const{roomId} = useParams();
+    const myMeeting =async(element) => {
+        const appID = 1927892384;
+        const serverSecret= "2f835a3898c9be8760deb6737a7648a3";
+        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomId, Date.now().toString(),"Anirudh Salaria");
+        const zc = ZegoUIKitPrebuilt.create(kitToken);
+        zc.joinRoom({
+            container: element,
+            sharedLinks: [
+                {
+                    name: 'Copy Link',
+                    url: `http://localhost:3000/room/${roomId}`,
+                },
+            ],
+            scenario: {
+                mode: ZegoUIKitPrebuilt.OneONoneCall,
+            },
+            showScreenSharingButton: true,
+        });
+    };
+
     return (
         <div>
             <>
-                <h1>Room</h1>
+                <div ref={myMeeting}/>
             </>
         </div>
     )
