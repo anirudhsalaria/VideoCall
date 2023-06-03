777aef8ffebf392515822f7e6e38fbed4e95e582
Anirudh Salaria <salariaanirudh@gmail.com>
2023-06-03 14:01:07 +0530

    testing

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -14,7 +14,7 @@ export default function HomePage() {
         <>
             {/* ----------------------------------------------------------------------------------------------------------------- */}
             <div>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
                     <div className="container-fluid">
                         <a className="navbar-brand" href="/">VideoCall</a>
                         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
