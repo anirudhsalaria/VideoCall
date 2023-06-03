97687962e5b7785efce1637647a0f91016358720
Anirudh Salaria <salariaanirudh@gmail.com>
2023-06-03 14:49:46 +0530

    changes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,9 @@
-import React, {useState, useCallback} from 'react'
+import React, { useState, useCallback } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 export default function HomePage() {
 
-    const[value, setValue] = useState();
+    const [value, setValue] = useState();
     const navigate = useNavigate();
 
     const handleJoinRoom = useCallback(() => {
@@ -16,7 +16,7 @@ export default function HomePage() {
             <div>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="/">VideoCall</a>
+                        <a className="navbar-brand" href="/">OneCamOne</a>
                         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
